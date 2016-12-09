91f065750acdb1f1fe3665ffcb72212d9db2f255
Sultan Magomedkadiev <smagomedkadiev@ccsteam.ru>
2016-12-09 18:18:29 +0300

    Add test xcode project.

---
+++ Dostavka05/Dostavka05/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dostavka05
+//
+//  Created by Sultan on 09/12/2016.
+//  Copyright © 2016 Sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
